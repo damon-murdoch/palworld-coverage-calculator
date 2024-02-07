b125850567361d48a06015bbbaeec06b744557d1
Scrubbs <damonmurdoch1@gmail.com>
2024-02-07 17:55:35 +1000

    Added html site template
    Added html site template

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+* Javascript code goes here :)
+* Any libraries should be placed in directory 'src' or 'js', 
+* and included in the project by adding something similar to
+* the following to 'index.html': 
+*/
+
+/*
+  <!-- Basic File Functionality Description -->
+  <script src="path/to/source/file.js"></script>
+*/
